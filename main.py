fdb4c20d62b60c0f6dee780130023f3eaf72e718
youshitsune <luka@youshitsune.me>
2023-03-30 07:02:33 +0200

    Implement TUI

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
+from textual.app import App, ComposeResult
+from textual.containers import Middle
+from textual.widgets import Header, Footer, Label, Static
+from textual.reactive import reactive
+from textual.widget import Widget
 import sys
 import random
 import os
 
-def main(filename):
+def flash(filename):
     with open(filename, "r") as f:
         ctx = f.read()
     ctx = ctx.split(";")
     flashcards = []
     for i in ctx:
-        flashcards.append((i.split("-")[0], i.split("-")[1]))
-    os.system("clear")
-    while True:
-        flashcard = random.choice(flashcards)
-        print()
-        print("Q:")
-        print(flashcard[0])
-        print()
-        user_input = input(":")
-        if user_input == "e" or user_input == "exit" or user_input == "quit" or user_input == "q":
-            break
+        flashcards.append((i.split("-")[0].strip(), i.split("-")[1].strip()))
+    return random.choice(flashcards)
+
+class CTX(Widget):
+    ctx = reactive("START")
+    index = reactive(0)
+    
+    def render(self) -> str:
+        return f"{self.ctx[self.index]}"
+
+
+class APP(App):
+
+    CSS_PATH = "main.css"
+
+    BINDINGS = [("enter", "next", "Next"), ("q", "exit", "Exit")]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield CTX()
+        yield Footer()
+
+    def action_next(self) -> None:
+        if self.query_one(CTX).index == 0:
+            self.query_one(CTX).index = 1
         else:
-            os.system("clear")
-            print()
-            print("A:")
-            print(flashcard[1])
-            print()
-            user_input = input(":")
-            if user_input == "e" or user_input == "exit" or user_input == "quit" or user_input == "q":
-                break
-            else:
-                os.system("clear")
+            self.query_one(CTX).ctx = flash(sys.argv[1])
+            self.query_one(CTX).index = 0
+
+    def action_exit(self) -> None:
+        sys.exit(0)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("HELP: python3 main.py <path_to_file>")
     else:
-        main(sys.argv[1])
+        app = APP()
+        ctx = flash(sys.argv[1])
+        app.run()
+            
+        
